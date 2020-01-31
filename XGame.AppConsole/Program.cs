7f7534f1559baff5e86f2e44b756f3827e24ad80
alisson.silva <alisson.silva@linx.com.br>
2020-01-31 18:01:27 -0300

    Criando um projeto para consumir o serviço

---
+++ XGame.AppConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using XGame.Domain.Arguments.Jogador;
+using XGame.Domain.Services;
+
+namespace XGame.AppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando...");
+
+            var service = new ServiceJogador();
+
+            Console.WriteLine("Criei umam isntancia o meu serviço");
+
+            AutenticarJogadorRequest request = new AutenticarJogadorRequest();
+            Console.WriteLine("Criei uma instancia do meu objeto de request");
+            request.Email = "paulo@paulo.com";
+            request.Senha = "12345678";
+
+            var response = service.AutenticarJogador(request);
+
+            Console.WriteLine("Serviço é valido -> " + service.IsValid());
+
+            service.Notifications.ToList().ForEach(x =>
+            {
+                Console.WriteLine(x.Message);
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
